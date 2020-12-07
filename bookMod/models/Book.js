8864a718942466288bf454babe16623ac586c683
Dilshodbeka <41250290+Dilshodbeka@users.noreply.github.com>
2020-12-07 13:44:15 +0100

    added new data

--- bookMod/models/Book.js
+++ bookMod/models/Book.js
@@ -1,11 +1,14 @@
 const uIdGenerator = require('node-unique-id-generator');
 
 class Book {
-    constructor(title= "", auther = "", desc = "", id = uIdGenerator.generateUniqueId()) {
-        this.title = title;
-        this.auther = auther;
-        this.desc = desc;
+    constructor(id = uIdGenerator.generateUniqueId(), title= "", description = "", authors = "", favorite = "", fileCover = "", fileName = "") {
         this.id = id;
+        this.title = title;
+        this.description = description;
+        this.authors = authors;
+        this.favorite = favorite;
+        this.fileCover = fileCover;
+        this.fileName = fileName;
     }
 }
 
