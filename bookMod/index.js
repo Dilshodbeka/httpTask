de86975aa33653d610351817825646fa2b158cae
Dilshodbeka <41250290+Dilshodbeka@users.noreply.github.com>
2020-12-10 13:12:33 +0100

    added multer

--- bookMod/index.js
+++ bookMod/index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser  = require('body-parser')
 
+const loggerMiddleWare = require('./middleware/logger')
+const errorMiddleWare = require('./middleware/error')
+
 const indexRouter = require('./routes/index')
 const bookRouter = require('./routes/book')
 
@@ -9,11 +12,14 @@ const app = express()
 
 app.use(bodyParser.urlencoded({extended : true}))
 app.use(cors())
+app.use(loggerMiddleWare)
 
-app.use('/public' , express.static(__dirname + 'public'))
+app.use('/files' , express.static(__dirname + 'public'))
 
 app.use('/', indexRouter)
 app.use('/api/book', bookRouter)
 
+
+app.use(errorMiddleWare)
 const PORT = process.env.PORT || 3000
 app.listen(PORT, console.log('starting', PORT))
