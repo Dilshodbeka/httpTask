37449f98f1e73a81e4f6b2f9f02a62d8fe82c0d2
Dilshodbeka <41250290+Dilshodbeka@users.noreply.github.com>
2020-12-10 12:00:49 +0100

    added download file

---
+++ bookMod/middleware/downloadFile.js
@@ -0,0 +1,24 @@
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'public/img')
+    },
+    filename: function (req, file, cb) {
+      cb(null, `${new Date().toISOString().replace(/:/g, '-')}-${file.originalname}`)
+    }
+  })
+
+const uploadType = ['book/pdf', 'book/fb', 'book/epub']
+
+const fileFilter = (req, file, cb) => {
+    if (uploadType.includes(file.mimetype)) {
+      cb(null, true)
+    } else {
+      cb(null, false)
+    }
+  };
+  
+  module.exports = multer({
+    storage, fileFilter
+  });
