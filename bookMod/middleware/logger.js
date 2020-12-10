3375082dbb8be0b1c16f8adf19c99c632a2ca91a
Dilshodbeka <41250290+Dilshodbeka@users.noreply.github.com>
2020-12-10 12:07:21 +0100

    logger

--- bookMod/middleware/logger.js
+++ bookMod/middleware/logger.js
@@ -1,1 +1,19 @@
+const fs = require('fs')
+const os = require('os')
 
+module.exports = (req, res, next) => {
+    let now = new Date();
+    let hour = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds(); 
+
+    const {method, url} = req
+    const usAgent = req.get('user-agent')
+
+    let data = `${hour}:${minutes}:${seconds} ${method}: ${url} user-agent: ${usAgent}`;
+
+    fs.appendFile('server.log', data + os.EOL, (err) =>{
+        if (err)  throw err
+    })
+    next();
+}
