ae0a386c0d03c28a3c448a5d033ea2b94fcf7b5c
Dilshodbeka <41250290+Dilshodbeka@users.noreply.github.com>
2020-12-15 12:07:44 +0100

    added ejs

--- bookMod/index.js
+++ bookMod/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser  = require('body-parser')
+let ejs = require('ejs')
 
 const loggerMiddleWare = require('./middleware/logger')
 const errorMiddleWare = require('./middleware/error')
@@ -10,13 +11,16 @@ const bookRouter = require('./routes/book')
 
 const app = express()
 
+app.set('view engine', 'ejs')
+
 app.use(bodyParser.urlencoded({extended : true}))
 app.use(cors())
 app.use(loggerMiddleWare)
 
-app.use('/files' , express.static(__dirname + 'public'))
 
+app.use('/files' , express.static(__dirname + 'public'))
 app.use('/', indexRouter)
+app.use('/book', bookRouter)
 app.use('/api/book', bookRouter)
 
 
